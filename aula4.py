89146f5ac1eb2279b03deeba0e483c3412d40520
Aluno <henriqueyan@hotmail.com>
2025-08-28 10:10:06 -0300

    aula 4

---
+++ aula4.py
@@ -0,0 +1,43 @@
+import numpy as np
+from PySide6 import QtOpenGLWidgets
+from PySide6.QtWidgets import *
+import matplotlib.pyplot as plt
+from OpenGL.GL import *
+import sys
+
+class Canvas(QtOpenGLWidgets.QOpenGLWidget):
+
+    def __init__(self):
+        super(Canvas,self).__init__()
+        self.setGeometry(100,100,600,400)
+        self.setWindowTitle("gldrawer")
+        self.m_w = 0
+        self.m_h = 0
+
+    def initializeGL(self):
+        glClearColor(1.0,1.0,1.0,1.0,)
+        glClear(GL_COLOR_BUFFER_BIT)
+    
+    
+    def resizeGL(self, w, h):
+        self.m_w = w
+        self.m_h = h
+        glViewport(0, 0, self.m_w, self.m_h)
+
+        glMatrixMode(GL_PROJECTION)
+        glOrtho(0.0,self.m_h, 0.0, self.m_h, -1.0, 1.0)
+
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+    
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget= Canvas()
+    widget.show()
+    sys.exit(app.exec())
+
+
