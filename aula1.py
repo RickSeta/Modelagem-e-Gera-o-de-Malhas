915401d4087bf2e1fb2a2bc4f5dc462b11614493
RickSeta <54965077+RickSeta@users.noreply.github.com>
2025-08-29 08:46:23 -0300

    RPN implementada

--- aula1.py
+++ aula1.py
@@ -4,14 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from rpn import RPN
-
-
 class MyWidget(QWidget, RPN):
     def __init__(self):
         super(MyWidget,self).__init__()
         self.pilha = []
         self.setGeometry(100,100,200,75)
-        self.setWindowTitle("myy")
+        self.setWindowTitle("RPN Henrique")
         self.r1 = QLabel("Termo a ser adicionado")
         self.t1 = QLineEdit()
         self.r2 = QLabel("RPN")
@@ -21,6 +19,8 @@ class MyWidget(QWidget, RPN):
         self.b2 = QPushButton("Multiplicacao")
         self.b3 = QPushButton("Divisao")
         self.b4 = QPushButton("Subtracao")
+        self.b5 = QPushButton("limpa")
+        self.b6 = QPushButton("<")
         self.exec = QPushButton("Executar")
         self.vbox = QVBoxLayout()
         self.vbox.addWidget(self.r1)
@@ -29,135 +29,118 @@ class MyWidget(QWidget, RPN):
         self.vbox.addWidget(self.appendB)
         self.vbox.addWidget(self.t2)
         
-        
         self.vbox.addWidget(self.b1)
         self.vbox.addWidget(self.b2)
         self.vbox.addWidget(self.b3)
         self.vbox.addWidget(self.b4)
+        self.vbox.addWidget(self.b5)
+        self.vbox.addWidget(self.b6)
         self.vbox.addWidget(self.exec)
         self.setLayout(self.vbox)
 
-        self.b1.clicked.connect((lambda x: self.addOp("+")))
-        self.b2.clicked.connect((lambda x: self.addOp("*")))
-        self.b3.clicked.connect((lambda x: self.addOp("/")))
-        self.b4.clicked.connect((lambda x: self.addOp("-")))
+        self.b1.clicked.connect((lambda x: self.addOp(" + ")))
+        self.b2.clicked.connect((lambda x: self.addOp(" * ")))
+        self.b3.clicked.connect((lambda x: self.addOp(" / ")))
+        self.b4.clicked.connect((lambda x: self.addOp(" - ")))
+        self.b5.clicked.connect((lambda x: self.clear()))
+        self.b6.clicked.connect((lambda x: self.back()))
         self.appendB.clicked.connect(self.addPilha)
         self.exec.clicked.connect(self.execute)
 
     def updateT2(self):
         self.t2.setText(" ".join(list(map(str,self.pilha))))
+
+    def clear(self):
+        self.pilha.clear()
+        self.updateT2()
+
+    def back(self):
+        self.pilha.pop()
+        self.updateT2()
     
     def addOp(self, op):
-        self.pilha.append(op)
+        self.pilha.append(op.strip(" "))
         self.updateT2()
         
-        
+    def parser(self):
+        text = self.t1.text()
+        temp = ""
+        for x in range(len(text)):
+            isOp = self.checkOp(text[x])
+            if(text[x] == " " or isOp):
+                if len(temp) > 0:
+                   self.pilha.append(temp)
+                temp = ""
+                if isOp:
+                    self.pilha.append(text[x])
+            else:
+                temp += text[x]
+
     def addPilha(self):
-        print(self.t1.text)
-        self.pilha.append(self.t1.text())
+        print(self.t1.text())
+        if not self.t1.text():
+            return
+        if(len(self.t1.text()) > 1):
+            self.parser()
+        else:
+            self.pilha.append(self.t1.text())
         self.t1.setText("")
         self.updateT2()
         print(self.pilha)
   
     def soma(self):
         lastTerm = self.pilha.pop()
-        if self.checkOp(lastTerm):
-            self.execute()
-            lastTerm = self.pilha.pop()
         secondToLastTerm = self.pilha.pop()
-        if self.checkOp(secondToLastTerm):
-            self.execute()
-            secondToLastTerm = self.pilha.pop()
         self.pilha.append(int(lastTerm) + int(secondToLastTerm))
-        
         self.updateT2()
     
-        
     def mult(self):
         lastTerm = self.pilha.pop()
         secondToLastTerm = self.pilha.pop()
         self.pilha.append(int(lastTerm) * int(secondToLastTerm))
         self.updateT2()
-    
-        
+     
     def div(self):
         secondToLastTerm = self.pilha.pop()
         lastTerm = self.pilha.pop()
         self.pilha.append(int(lastTerm) / int(secondToLastTerm))
         self.updateT2()
     
-        
     def sub(self):
         secondToLastTerm = self.pilha.pop()
         lastTerm = self.pilha.pop()
         self.pilha.append(int(lastTerm) - int(secondToLastTerm))
         self.updateT2()
-    
+
     def execute(self):
         
         if(len(self.pilha) < 3):
-            print("fim")
+            print(self.pilha[-1])
             return
-        op = self.pilha[-1]
+        if(self.checkOp(self.pilha[-1])):
+            op = self.pilha.pop()
+
+        lastTerm = self.pilha[-1]
+        secondToLastTerm = self.pilha[-2]
         
+        if self.checkOp(secondToLastTerm):
+            temp = self.pilha.pop()
+            self.execute()
+            self.pilha.append(temp)
+        if self.checkOp(lastTerm):
+            self.execute()
+
         if(op == "+"):
-            self.pilha.pop()
             self.soma()        
         if(op == "-"):
-            self.pilha.pop()
             self.sub()        
         if(op == "/"):
-            self.pilha.pop()
             self.div()        
         if(op == "*"):
-            self.pilha.pop()
-            self.mult()
+            self.mult() 
     
     def checkOp(self,op):
         return op in ["+", "-", "*", "/"]
-    # def soma(self):
-    #     print("clicado")
-    #     a = float(self.t1.text())
-    #     b = float(self.t2.text())
-    #     self.t3.setText(str(a+b))
-
-    
-    # def mult(self):
-    #     print("clicado")
-    #     a = float(self.t1.text())
-    #     b = float(self.t2.text())
-    #     self.t3.setText(str(a*b))
-
-
-    # def div(self):
-    #     print("clicado")
-    #     a = float(self.t1.text())
-    #     b = float(self.t2.text())
-    #     self.t3.setText(str(a/b))
-
-
-    # def sub(self):
-    #     print("clicado")
-    #     a = float(self.t1.text())
-    #     b = float(self.t2.text())
-    #     self.t3.setText(str(a-b))
-
-
-
-def window():
-    app = QApplication(sys.argv)
-    w = QWidget()
-    b = QLabel(w)
-    b.setText("Ola mundo")
-
-    b.setAlignment(Qt.AlignCenter)
-
-    w.setGeometry(100,100,200,75)
-    b.move(50,20)
-    w.setWindowTitle("Xum")
-    w.show()
-    sys.exit(app.exec_())
-
 
 app = QApplication(sys.argv)
 widget = MyWidget()
