790bac9e319f91023aa462e93c51e961b43132d7
Aluno <henriqueyan@hotmail.com>
2025-09-04 10:48:48 -0300

    scaling last class

--- aula6.py
+++ aula6.py
@@ -15,7 +15,7 @@ class Canvas(QtOpenGLWidgets.QOpenGLWidget):
         self.m_h = 0
 
     def initializeGL(self):
-        glClearColor(1.0,1.0,1.0,1.0,)
+        glClearColor(0.0,0.0,0.0,1.0)
         glClear(GL_COLOR_BUFFER_BIT)
     
     
@@ -25,27 +25,38 @@ class Canvas(QtOpenGLWidgets.QOpenGLWidget):
         glViewport(0, 0, self.m_w, self.m_h)
 
         glMatrixMode(GL_PROJECTION)
-        glOrtho(0.0,self.m_w, 0.0, self.m_h, -1.0, 1.0)
-
+        glLoadIdentity()
+        glOrtho(-1000.0,1000, -1000.0, 1000, -1.0, 1.0)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
     
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT)
         glShadeModel(GL_SMOOTH)
-        xA = self.m_w / 3.0
-        yA = self.m_h / 3.0
-        xB = self.m_w * (2.0 / 3.0)
-        yB = self.m_h / 3.0
-        xC = self.m_w / 2.0
-        yC = self.m_h  * (2.0 / 3.0)
-        glBegin(GL_TRIANGLES)
-        glColor3f(1.0, 0.0, 0.0)
-        glVertex2f(xA, yA)
-        glColor3f(0.0, 1.0, 0.0)
-        glVertex2f(xB, yB)
-        glColor3f(0.0, 0.0, 1.0)
-        glVertex2f(xC, yC)
+
+
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        
+        glColor3f(1.0,1.0,1.0)
+        glLineWidth(2.0)
+        glBegin(GL_LINES)
+        glVertex2i(-1000, 0)
+        glVertex2i(1000, 0)
+        glVertex2i(0, -1000)
+        glVertex2i(0, 1000)
+        glEnd()
+
+        glTranslatef(40.0, 40.0, 0.0)
+        glRotatef(30.0,0.0,0.0,1.0)
+        glScalef(2.0,2.0,0.0)
+
+        glBegin(GL_QUADS)
+        glColor3f(1.0,1.0,1.0)
+        glVertex2i(-20, -20)
+        glVertex2i(20 , -20)
+        glVertex2i(20 , 20)
+        glVertex2i(-20, 20)
         glEnd()
     
 if __name__ == '__main__':
