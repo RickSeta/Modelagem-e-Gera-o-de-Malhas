06d67b4d4a1ae7b62423eee718a9ed5d6b8d44c9
RickSeta <54965077+RickSeta@users.noreply.github.com>
2025-09-03 23:53:10 -0300

    aula4

--- aula4.py
+++ aula4.py
@@ -33,10 +33,20 @@ class Canvas(QtOpenGLWidgets.QOpenGLWidget):
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT)
         glShadeModel(GL_SMOOTH)
-        xA
-        yA
-        zA
-
+        xA = self.m_w / 3.0
+        yA = self.m_h / 3.0
+        xB = self.m_w * (2.0 / 3.0)
+        yB = self.m_h / 3.0
+        xC = self.m_w / 2.0
+        yC = self.m_h  * (2.0 / 3.0)
+        glBegin(GL_TRIANGLES)
+        glColor3f(1.0, 0.0, 0.0)
+        glVertex2f(xA, yA)
+        glColor3f(0.0, 1.0, 0.0)
+        glVertex2f(xB, yB)
+        glColor3f(0.0, 0.0, 1.0)
+        glVertex2f(xC, yC)
+        glEnd()
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
